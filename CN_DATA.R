4ac5b74bcf0f37939bd1299a41dc60b3d37d85e7
unknown <BlueCarbon@Bluecarbon-asus.du.deakin.edu.au>
2019-08-21 17:10:51 +1000

    Script to combine and merge CN data spreadsheets produced by CN analyzer (EURO 3000. Extra plots added.

--- CN_DATA.R
+++ CN_DATA.R
@@ -10,7 +10,7 @@ library("tidyverse")
 #YOUR SAMPLE_ID (I named it CNCode) should be consistent across all your spreadsheets.
 
 #Specify path to folder where you store youre CN spreadsheets and list them all:
-files <- list.files(path = "C:/Users/BlueCarbon/Documents/0.Deakin Uni/R/BCL_R/CN_DATA_12jun19",
+files <- list.files(path = "C:/Users/BlueCarbon/Documents/00DeakinUni/R/BCL_R/CN_DATA_12jun19",
                     pattern = "*.xls", full.names = T)
 
 #Create function to export data from sheet = 1 (Sample Table)
@@ -20,9 +20,8 @@ ReadCN_smp <- function(x) read_xlsx (path = x,
                                      range = cell_cols("C:E"))
 
 #Export "Sample Table" data from all files in your folder:
-tbl1 <- sapply(files, 
-             ReadCN_smp, simplify=FALSE) %>% 
-             bind_rows(.id = "id1")
+tbl1 <- sapply(files, ReadCN_smp, simplify=FALSE) %>%
+         bind_rows(.id = "id1")
 
 
 #Create function to export data from sheet = 2 (Element%)
@@ -52,18 +51,21 @@ anyDuplicated(CN_DATA_clean$CNCode)#Should be 0!!!
 #You have to decide what to do with duplicates (e.g., average them, remove them)
 
 #Merge new CN data with your MASTER file:
-MASTER_DATA <- read_csv("CN.csv")
+MASTER_DATA <- read.csv("CN.csv")
 NewDATA <- left_join(MASTER_DATA, CN_DATA_clean, by = "CNCode")
 
-dim(MASTER_DATA)#2233 rows   36 cols
-dim(NewDATA)#2233 rows  40 cols
+dim(MASTER_DATA)#2233 rows   44 cols
+dim(NewDATA)#2233 rows  48 cols
+#write.csv(NewDATA, file = "CN_NewDATA.csv")
 
 #Plot======
-NewDATA <- NewDATA [ !is.na(NewDATA$site),]
+NewDATA <- NewDATA [ !is.na(NewDATA$site),] #remove NA-s
 NewDATA$habitat <- factor(NewDATA$habitat,
                    levels = c("Saltmarsh","Mangrove","Seagrass"))
 NewDATA <-filter(NewDATA, HCl_Bubbles =="no")
-names(NewDATA)
+
+#PLOT CN-DATA=====
+#Plot By ecosystem type:
 ggplot(NewDATA, aes(x = CPb, y = C.percent,color=habitat)) +
   geom_boxplot() +
   facet_grid(.~habitat)+ geom_jitter(alpha = 0.4)+
@@ -77,8 +79,8 @@ ggplot(NewDATA, aes(x = CPb, y = C.percent,color=habitat)) +
         legend.text = element_text(size = 16),
         strip.text=element_text(size=16),
                 strip.background =  element_rect(fill = "white"))
-
-ggplot(NewDATA, aes(x = habitat, y = C.percent,color=habitat)) +
+#Plot By site:
+ggplot(NewDATA, aes(x = habitat, y = C.percent, color=habitat)) +
   geom_boxplot() +
   facet_grid(.~site)+
   ylab("Organic Carbon (%)") + xlab("") +
@@ -91,3 +93,35 @@ ggplot(NewDATA, aes(x = habitat, y = C.percent,color=habitat)) +
         legend.text = element_text(size = 16),
         strip.text=element_text(size=16),
         strip.background =  element_rect(fill = "white"))
+
+#Plot By Depth_Range:=======
+#Subset saltmarsh and compare the C-stock between top 10cm slices:
+
+slices <- select(NewDATA,site,core, habitat,C.percent, DepthRange.cm) %>%
+  filter(habitat=="Saltmarsh") %>%
+ filter( DepthRange.cm== "00to02" |
+           DepthRange.cm== "02to05" |
+           DepthRange.cm== "05to10") %>%
+  group_by(core) %>%  mutate(Rough10 = mean (C.percent)) 
+
+rough10 <- select(slices,core, Rough10, habitat) %>%
+  rename(C.percent = Rough10) %>%
+  mutate(DepthRange.cm = "Rough10")
+old10 <-  select(slices, core, C.percent, habitat, DepthRange.cm)
+all10 <- rbind (old10, rough10)
+
+
+ggplot(all10, aes(x = habitat, y = C.percent, color=habitat)) +
+  geom_boxplot(outlier.shape = NA) +
+  facet_grid(.~DepthRange.cm)+
+  ylab("Organic Carbon (%)") + xlab("")+
+  theme_bw() + geom_jitter(alpha = 0.7)+
+  theme(axis.text.y = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        legend.position = "none",
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16),
+        strip.text=element_text(size=16),
+        strip.background =  element_rect(fill = "white"))
+
+
