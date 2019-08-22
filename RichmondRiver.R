d15422182ca16c747c352f59d52d31a5c6f1ae1e
unknown <BlueCarbon@Bluecarbon-asus.du.deakin.edu.au>
2019-08-22 11:54:14 +1000

    Analysis of data from Richmond River Project. Looking at belowground and aboveground carbon stock. Average max core depths output.

--- RichmondRiver.R
+++ RichmondRiver.R
@@ -16,7 +16,7 @@ belowSum <- select(bb, Site, Site_Core, Carbon_stock_in_section_Mg_Cha)%>%
   mutate (Stock = "Belowground")
 belowSum
 
-#Compute mean +- SE for aboveground C-Stock:
+#Compute mean +- SE for aboveground C-Stock:=========
 aboveSum <- select (aa, Site, Total_Aboveground_Biomass_kg_100m2) %>%
   mutate(Total_Aboveground_Biomass_Mg_ha = Total_Aboveground_Biomass_kg_100m2 / 10 )%>% #1 kg/100m2 = 0.1 tonnes/ha
   gather(key = treat, value = mass, Total_Aboveground_Biomass_Mg_ha) %>% 
@@ -27,12 +27,11 @@ aboveSum <- select (aa, Site, Total_Aboveground_Biomass_kg_100m2) %>%
             SE= SD / sqrt(N)) %>%
   mutate (Stock = "Aboveground")
 
-
-#Join above and below Stocks:
+#Join/Plot above and below Stocks:=====
 ab <- rbind (aboveSum,belowSum) %>%
   mutate(AV= ifelse(Stock =="Aboveground",AV,AV*-1))#turn belowground negative
 
-#Draw a figure:=======
+#Draw a figure:
 MyBreaks <- c(-300, -200,-100, -50, 0, 50, 100, 200,300 ,400,500)
 
 ggplot(ab, aes(x=Site, y=AV, fill = Stock))+
@@ -52,7 +51,56 @@ ggplot(ab, aes(x=Site, y=AV, fill = Stock))+
          legend.title = element_text(face = "italic",size=16),
          plot.title = element_text(lineheight=1.2, face="bold",size=20))
 
-#Histogram:=======
+#Fig of Below carbon by treatment:====
+TreatSum <- select(bb, Site, Treatment, Site_Core, Carbon_stock_in_section_Mg_Cha)%>%
+  filter(Treatment != "Saltmarsh natural") %>%
+  group_by(Site_Core) %>% #to sum total Carbon Stock per core.
+  mutate(TotalPerCore = sum (Carbon_stock_in_section_Mg_Cha)) %>%
+  group_by(Treatment) %>%
+  summarise(AV = mean(TotalPerCore, na.rm = T),
+            SD = sd(TotalPerCore),
+            N = length(TotalPerCore),
+            SE = SD / sqrt(N)) %>%
+  mutate (Stock = "Belowground")
+TreatSum
+ggplot(TreatSum, aes(x=Treatment, y=AV, fill = Treatment))+
+  geom_bar(position="identity", stat="identity") +
+geom_errorbar( aes(ymin= AV+SE, ymax = AV-SE), width=.4) +
+  ylab("Organic Carbon Stock (Mg/ha)")+
+  theme(axis.text.x = element_text(size=16),
+        axis.title.x = element_blank(),
+        legend.position = "none")+
+  ggtitle("Richmond River Project",
+          subtitle = "Mean Carbon stock by treatment (whole core)")
+  
+  
+#Fig of Below carbon by treatment and Depth_Range:====
+levels(bb$Depth_Range)#"000_015" "015_030" "030_050" "050_075" "075_100" "100_125"
+unique(bb$Depth_to)#"000_015" "015_030" "030_050" "050_075" "075_100" "100_125"
+
+DepthTreatSum <- select(bb, Site,Site_Core, Treatment,Depth_to, Depth_Range, Site_Core, Carbon_stock_in_section_Mg_Cha)%>%
+  filter(Treatment != "Saltmarsh natural") %>%
+  filter(Depth_to <= 50 ) %>%
+  group_by( Depth_Range,Treatment) %>% #to sum total Carbon Stock per core.
+  summarise(AV = mean(Carbon_stock_in_section_Mg_Cha, na.rm = T),
+            SD = sd(Carbon_stock_in_section_Mg_Cha),
+            N = length(Carbon_stock_in_section_Mg_Cha),
+            SE = SD / sqrt(N)) %>%
+  mutate (Stock = "Belowground")
+DepthTreatSum
+
+ggplot(DepthTreatSum, aes(x=Treatment, y=AV, fill = Treatment))+
+  geom_bar(position="identity", stat="identity") +
+  geom_errorbar( aes(ymin= AV+SE, ymax = AV-SE), width=.4) +
+  facet_grid(Depth_Range~.)+
+  theme_bw () +
+  ggtitle("Richmond River Project",
+          subtitle = "Mean Carbon stock by Depth_Range till 50 cm")+
+  ylab("Organic Carbon Stock (Mg/ha)")+
+    theme(axis.text.x = element_text(),
+        legend.position = "none")
+
+#Histogram: of Carbon across Depth_Ranges=======
 bbb <- select(bb,Depth_Range, Site, Site_Core, Carbon_stock_in_section_Mg_Cha)%>%
   group_by(Site_Core) %>% #to sum total Carbon Stock per core.
   mutate(TotalPerCore = sum (Carbon_stock_in_section_Mg_Cha)) 
@@ -63,4 +111,17 @@ ggplot(bb, aes(Carbon_stock_in_section_Mg_Cha, fill = Treatment))+
         axis.text.y=element_text(size=12),
         axis.title.y=element_text(size=20),
         legend.position = "bottom")
+
+#Average Max Depth Reached=====
+MaxDepth <- select(bb,Site_Core, Depth_to)%>%
+  group_by(Site_Core) %>% #to sum total Carbon Stock per core.
+  slice( which.max(Depth_to)) %>% #slice out rows with max Depth_to 
+  ungroup() %>%
+  mutate(Average_Max_Depth = mean(Depth_to, na.rm = T),
+            SD = sd(Depth_to),
+            N = length(Depth_to),
+            SE = SD / sqrt(N)) 
+range (MaxDepth$Depth_to)#38 110
+length (MaxDepth[ MaxDepth$Depth_to < 50,]) # 6 cores were below 50 cm
+100 - (6/48 * 100) #87.5% of cores were longer than 50 cm.
 
